aa44f9d33f0c6f4e5948446ae3321f14de85d50a
James Wu <jameswlb01@gmail.com>
2024-11-18 19:11:24 +0800

    Add clear statment and add more arguments when chaning operators

--- myscript.js
+++ myscript.js
@@ -2,40 +2,69 @@ let buttons = document.querySelectorAll(".calculator-button")
 let num1 = '';
 let num2 = '';
 let operator = null;
-let hasOperator = false;
+let operator_previous = null;
 
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
+        // Calculate answer using operate 
+        if (button.id === "equals" || 
+            (num1 && num2 && operator)
+        ) {
+            let answer = operate(num1,num2,operator);
+            console.log(answer);
+
+            // Reset the num1, num2, and opeartor
+            num1 = answer;
+            num2 = '';
+            operator_previous = operator;
+            operator = null;
+        }
+
+        // first condition: Clear out all variables
+        if (button.id === "clear") {
+            num1 = '';
+            num2 = '';
+            operator = null;
+            operator_previous = null;
+        }
 
         // Update num1 and num2
         if (button.id === "number") {
-            if (hasOperator === false) {
+            if (operator === null) {
                 num1 = num1 + button.textContent;
-            } else if (hasOperator === true) {
+            } else {
                 num2 = num2 + button.textContent;
             }
         }
 
         // Change operator status 
         if (button.id === "symbol") {
-            if (num1 !== null && operator === null) {
-                hasOperator = true;
+            if (num1 !== null && num2 === '' && operator === null) {
+                // Case 1: num1 exist
                 operator = button.textContent;
-            } else if (operator) {
-                let answer = operate(num1,num2,operator);
-                console.log(answer);
+            } else if (num1 !== null && operator !== null && operator_previous === null) {
+                // Case 2: num1 and operator exist
+                num2 = num1;
+            } else if (num1 !== null && operator === null && operator_previous !== null) {
+                // Case 3: num1 and previous operator exist
+                num2 = num1;
+                operator = operator_previous;
             }
         }
-        
+
         // Debugging Line
         console.log(`
             The id of the button is ${button.id}
             num1 is ${num1}
             num2 is ${num2}
-            opeartor is ${operator}`)     
+            operator is ${operator}
+            operator-tmp is ${operator_previous}`)  
+   
     });
 });
+
+
 
 function operate(num1, num2, symbol) {
     // Debugging line
@@ -44,10 +73,10 @@ function operate(num1, num2, symbol) {
     if (symbol === '+') {
         return (parseInt(num1, 10) + parseInt(num2, 10));
     } else if (symbol === '-') {
-        return (parseInt(num1, 10) + parseInt(num2, 10));
+        return (parseInt(num1, 10) - parseInt(num2, 10));
     } else if (symbol === '*') {
-        return (parseInt(num1, 10) + parseInt(num2, 10));
+        return (parseInt(num1, 10) * parseInt(num2, 10));
     } else if (symbol === '/') {
-        return (parseInt(num1, 10) + parseInt(num2, 10));
+        return (parseInt(num1, 10) / parseInt(num2, 10));
     } 
 }
